8f0c76d8e4d999a73b9e6da3d681ba2fd4607df2
Jordan Arnesen <jordan101112@gmail.com>
2014-12-28 20:14:45 -0800

    Use javascript to randomize descriptive words

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,21 @@
-var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'EXTRAJORDANARY';
+var randomNumber, groupWord, thingWord, actionWord, adjectiveWord;
+// var myHeading = document.querySelector('h1');
+
+randomNumber = [Math.floor(Math.random() * words['groups'].length)];
+groupWord = words['groups'][randomNumber];
+
+randomNumber = [Math.floor(Math.random() * words['things'].length)];
+thingWord = words['things'][randomNumber];
+
+randomNumber = [Math.floor(Math.random() * words['actions'].length)];
+actionWord = words['actions'][randomNumber];
+
+randomNumber = [Math.floor(Math.random() * words['adjectives'].length)];
+adjectiveWord = words['adjectives'][randomNumber];
+
+document.querySelector('h1').innerHTML = 'EXTRAJORDANARY';
+document.querySelector('.group-word').innerHTML = groupWord;
+document.querySelector('.thing-word').innerHTML = thingWord;
+document.querySelector('.thing-word2').innerHTML = thingWord;
+document.querySelector('.action-word').innerHTML = actionWord;
+document.querySelector('.adjective-word').innerHTML = adjectiveWord;
